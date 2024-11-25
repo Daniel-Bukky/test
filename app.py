53a53b5b8e35162f824aa698a7d4ffb8ab5dd60e
Daniel-Bukky <daniel.bukky@gmail.com>
2024-11-25 20:59:40 +0200

    FLASK

--- app.py
+++ app.py
@@ -1,3 +1,68 @@
-'''
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente rem at esse accusamus, facere sit voluptas pariatur ipsa quae, suscipit voluptatem adipisci delectus facilis veniam quidem molestiae sint sunt asperiores excepturi officia soluta quas possimus earum. Molestias obcaecati labore dolorum, dolores blanditiis eaque officiis corporis fugit voluptate harum officia eos earum nesciunt eum omnis! Neque officia odio et qui voluptatem cupiditate dignissimos ipsum est nemo placeat id, mollitia eligendi illo aliquam dicta minima reiciendis similique, alias dolor eius pariatur excepturi laborum. Ut, laborum qui voluptatibus sint esse amet soluta reprehenderit optio quod aliquam tenetur ipsum veritatis dicta. Recusandae, impedit at?
-'''
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+
+def read_json_file():
+    with open("data.json", "r") as file:
+        return json.load(file)
+
+
+def write_json_file(data):
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+@app.route("/", methods=['GET'])
+def get_all_data():
+    data = read_json_file()
+    return jsonify(data)
+
+
+@app.route("/data/<int:item_id>", methods=['GET'])
+def get_data_by_id(item_id):
+    data = read_json_file()
+    entry = next((item for item in data if item['id'] == item_id), None)
+    if entry:
+        return jsonify(entry)
+    return jsonify({"message": "Item not found"}), 404
+
+
+@app.route('/', methods=['POST'])
+def add_data():
+    new_entry = request.get_json()
+    data = read_json_file()
+    data.append(new_entry)
+    write_json_file(data)
+    return jsonify({'message': 'Entry added successfully'}), 201
+
+
+@app.route("/data/<int:item_id>", methods=['DELETE'])
+def delete_data_by_id(item_id):
+    data = read_json_file()
+    entry = next((item for item in data if item['id'] == item_id), None)
+    if entry:
+        data.remove(entry)
+        write_json_file(data)
+        return jsonify({"message": f"Entry with id {item_id} deleted successfully"}), 200
+    return jsonify({"message": "Item not found"}), 404
+
+
+@app.route("/data/<int:item_id>", methods=['PATCH'])
+def patch_data_by_id(item_id):
+    data = read_json_file()
+    entry = next((item for item in data if item['id'] == item_id), None)
+    if entry:
+        update_data = request.get_json()
+        for key, value in update_data.items():
+            if key in entry:
+                entry[key] = value
+
+        write_json_file(data)
+        return jsonify({"message": f"Entry with id {item_id} updated successfully"}), 200
+    return jsonify({"message": "Item not found"}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
